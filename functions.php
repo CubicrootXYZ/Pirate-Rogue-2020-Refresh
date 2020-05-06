d56573f8d56b1e1ea71b667f15b5e5b2723252bd
Cubicroot <24812442+CubicrootXYZ@users.noreply.github.com>
2020-05-06 20:26:51 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 /* enqueue styles og parent theme */
 function parent_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', time());
     
 	// Dequeue Style from pirate-crew, this theme has its own style for it
     wp_dequeue_style('pirate-crew');
