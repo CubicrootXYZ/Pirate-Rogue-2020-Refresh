ae74916db83fcfb46ec46ac53843c749ccb25468
Cubicroot <24812442+CubicrootXYZ@users.noreply.github.com>
2020-05-29 15:40:14 +0200

    .

---
+++ custom_shortcodes.php
@@ -0,0 +1,15 @@
+<?php
+
+// Add all shortcodes
+add_shortcode('topicrect', 'topicrect_func');
+
+function topicrect_func($atts, $content = null) {
+    // set up default parameters
+    extract(shortcode_atts(array(
+        'image' => '',
+        'url' => '#',
+        'title' => 'Use title=Title'
+       ), $atts));
+    return "<div style='background-image: url(".$image.")' class=''>".$title."</div>"
+}
+?>
