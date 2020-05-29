fa724fe0ade011b3007ebf72da4f6ea0755e19be
Cubicroot <24812442+CubicrootXYZ@users.noreply.github.com>
2020-05-29 17:19:28 +0200

    .

--- custom_shortcodes.php
+++ custom_shortcodes.php
@@ -3,6 +3,7 @@
 // Add all shortcodes
 add_shortcode('topicrect', 'topicrect_func');
 add_shortcode('flexrow', 'flexrow_func');
+add_shortcode('hpic', 'hpic_func');
 
 function topicrect_func($atts, $content = null) {
     // set up default parameters
@@ -12,7 +13,7 @@ function topicrect_func($atts, $content = null) {
         'title' => 'Use title=Title',
         'width' => "31%"
        ), $atts));
-    return "<div onclick='location.href=\"".$url."\";' style='background-image: url(".$image."); width:".$width.";' class='topicrect'><div class='topicrect-title'>".$title."</div><div class='topicrect-content'>".$content."</div></div>";
+    return "<div onclick='location.href=\"".$url."\";' style='background-image: url(\"".$image."\"); width:".$width.";' class='topicrect'><div class='topicrect-title'>".$title."</div><div class='topicrect-content'>".$content."</div></div>";
 }
 function flexrow_func($atts, $content = null) {
     // set up default parameters
@@ -20,5 +21,13 @@ function flexrow_func($atts, $content = null) {
         'image' => ''
        ), $atts));
     return do_shortcode("<div class='custom-flexrow'>".$content."</div>");
+}
+function flexrow_func($atts) {
+    // set up default parameters
+    extract(shortcode_atts(array(
+        'image' => '',
+        'title' => ''
+       ), $atts));
+    return do_shortcode("<div class='custom-hpic' style='background-image: url(\"".$image."\")'><div class='custom-hpic-inner' ><h2>".$title."</h2></div></div>");
 }
 ?>
