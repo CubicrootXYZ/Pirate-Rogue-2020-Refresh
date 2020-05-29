14c597119cd0e918793ed61384047822e906efab
Cubicroot <24812442+CubicrootXYZ@users.noreply.github.com>
2020-05-29 16:33:22 +0200

    .

--- custom_shortcodes.php
+++ custom_shortcodes.php
@@ -10,6 +10,6 @@ function topicrect_func($atts, $content = null) {
         'url' => '#',
         'title' => 'Use title=Title'
        ), $atts));
-    return "<a href='".$url."'><div style='background-image: url(".$image.")' class='topicrect'><div class='topicrect-title'>".$title."</div><div class='topicrect-content'>".$content."</div></div></a>";
+    return "<a class='topicrect-url' href='".$url."'><div style='background-image: url(".$image.")' class='topicrect'><div class='topicrect-title'>".$title."</div><div class='topicrect-content'>".$content."</div></div></a>";
 }
 ?>
