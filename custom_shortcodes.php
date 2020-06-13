1438008ea5d349b97e50beb17351497d435133e1
Cubicroot <github@cubicroot.xyz>
2020-06-13 16:57:12 +0200

    fixed issue

--- custom_shortcodes.php
+++ custom_shortcodes.php
@@ -13,7 +13,8 @@ function topicrect_func($atts, $content = null) {
         'title' => 'Use title=Title',
         'width' => "33%"
        ), $atts));
-    return "<div onclick='location.href=\"".$url."\";' style='background-image: url(\"".$image."\"); width:".$width-2.";' class='topicrect'><div class='topicrect-title'>".$title."</div><div class='topicrect-content'>".$content."</div></div>";
+       $width=$width-2;
+    return "<div onclick='location.href=\"".$url."\";' style='background-image: url(\"".$image."\"); width:".$width.";' class='topicrect'><div class='topicrect-title'>".$title."</div><div class='topicrect-content'>".$content."</div></div>";
 }
 function flexrow_func($atts, $content = null) {
     // set up default parameters
